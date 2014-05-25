0b3de72dcf70a00e26576cceae2c1e3cf7aa3b3d
Eugene Bulkin <ebulkin@doubleaw.com>
2014-05-25 00:15:00 +0000

    Fix NHL and other schedules

--- modules/schedules.js
+++ modules/schedules.js
@@ -3,8 +3,6 @@ var cheerio = require('cheerio');
 var humanize = require('humanize');
 var moment = require('moment-timezone');
 
-var abbrHash = require('./abbrHash.json');
-
 var bold = function(str) {
   return '\u0002' + str + '\u000F';
 };
@@ -12,13 +10,6 @@ var bold = function(str) {
 var int = function(str) {
   return parseInt(str, 10);
 };
-
-var toAbbr = function(team) {
-  return Object.keys(abbrHash).filter(function(key) {
-    return abbrHash[key].toLowerCase() === team.toLowerCase();
-  })[0];
-};
-
 var zipHash = function(keys, values) {
   var obj = {};
   keys.forEach(function(key, i) {
@@ -27,20 +18,23 @@ var zipHash = function(keys, values) {
   return obj;
 };
 
+var getDate = function(data) {
+  var ds = data.split(' ');
+  var data_date = ds.pop();
+  if (data_date === 'tomorrow') {
+    date = moment().tz('America/New_York').add(moment.duration(1, 'day'));
+  } else if(data_date.match(/^[\d]{8}$/)) {
+    date = moment(data_date, 'YYYYMMDD', 'America/New_York');
+  } else {
+    date = moment().tz('America/New_York');
+  }
+  return date;
+};
+
 module.exports = {
   "mls": function(data, user, target) {
-    var date, ds = data ? data.split(" ") : [];
-    if(ds.length > 0) {
-      // requested a date!
-      date = ds[0];
-      if(!date.match(/^[\d]{8}$/)) {
-        this.client.say(target, "The date specified is incorrectly formatted. Your request should look like ,mls [YYYYMMDD]");
-        return user.nickname + " asked for the MLS schedule for '" + data + "', but the date was badly formatted.";
-      }
-      date = moment(date, "YYYYMMDD");
-    } else {
-      date = moment();
-    }
+    var date = getDate(data);
+    var showAll = data.split(' ')[0] === '*';
     var url = 'http://www.espnfc.com/scores/_/date/' + date.format('YYYYMMDD') + '/league/usa.1';
     HTTP.read(url).then(function(b) {
       var $ = cheerio.load(b.toString());
@@ -58,17 +52,18 @@ module.exports = {
           dateStr = moment(dateStr, 'MMMM D, HH:mm').tz('America/New_York').format('h:mm A') + ' ET';
         }
         var scores = $(row.children[2]).text().replace(/[^0-9\-]/g,'').split('-').map(int);
+        var tm1 = $(row.children[1]).text().trim(), tm2 = $(row.children[3]).text().trim();
         if(scores.length > 1) {
-          var tm1 = $(row.children[1]).text().trim() + ' ' + scores[0];
-          var tm2 = $(row.children[3]).text().trim() + ' ' + scores[1];
+          tm1 += ' ' + scores[0];
+          tm2 += ' ' + scores[1];
           if(scores[0] < scores[1]) {
             tm2 = bold(tm2);
           } else if(scores[0] > scores[1]) {
             tm1 = bold(tm1);
           }
-          return tm1 + ' ' + tm2 + ' (' + dateStr + ')';
+          return tm1 + ' vs. ' + tm2 + ' (' + dateStr + ')';
         } else {
-          return $(row.children[1]).text().trim() + ' vs ' + $(row.children[3]).text().trim() + ' (' + dateStr + ')';
+          return tm1 + ' vs. ' + tm2 + ' (' + dateStr + ')';
         }
       }).join("\n");
     }).then(function(sched) {
@@ -77,39 +72,41 @@ module.exports = {
     }.bind(this));
   },
   "nhl": function(data, user, target) {
-    var date;
-    if (data === 'tomorrow') {
-      date = moment().tz('America/New_York').add(moment.duration(1, 'day'));
-    } else {
-      date = moment().tz('America/New_York');
-    }
-    HTTP.read('http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp').then(function(b) {
-      var games = JSON.parse(b.toString('utf8').replace('loadScoreboard(','').slice(0,-1)).games;
-      return games.filter(function(game) {
-        var d = game.ts.split(' ').pop();
-        if(d.toLowerCase() === 'today') {
-          d = moment().tz('America/New_York');
-        } else {
-          d = moment(d, 'M/D', 'America/New_York');
+    var date = getDate(data);
+    var showAll = data.split(' ')[0] === '*';
+    var url = 'http://live.nhle.com/GameData/GCScoreboard/' + date.format('YYYY-MM-DD') + '.jsonp';
+    HTTP.read(url).then(function(b) {
+      var json = JSON.parse(b.toString('utf8').trim().match(/^loadScoreboard\((.+?)\)$/)[1]);
+      var games = json.games;
+      if(games.length === 0) {
+        return 'No games found.';
+      }
+      return games.map(function(game) {
+        var tm1 = game.ata, tm2 = game.hta;
+
+        var time = game.bs + ' ET';
+        if (['final', 'progress'].indexOf(game.bsc) > -1) {
+          time = game.bs;
+          var s1 = game.ats, s2 = game.hts;
+          tm1 += ' ' + s1;
+          tm2 += ' ' + s2;
+          if(s1 < s2) {
+            tm2 = bold(tm2);
+          } else if(s1 > s2) {
+            tm1 = bold(tm1);
+          }
         }
-        return d.format('YYYYMMDD') === date.format('YYYYMMDD');
-      }).map(function(game) {
-        return toAbbr(game.atv) + " @ " + toAbbr(game.htv) + ' (' + game.bs + ' ET) [' + game.ustv.split(',').concat(game.catv.split(',')).join(', ') + ']';
+        var stations = game.usnationalbroadcasts.split(', ').concat(game.canationalbroadcasts.split(', ')).join(', ');
+        return tm1 + " @ " + tm2 + ' (' + time + ') [' + stations + ']';
       }).join("\n");
     }).then(function(sched) {
       this.client.say(target, sched);
       return user.nickname + ' asked for the NHL schedule.';
-    }.bind(this));
+    }.bind(this)).done();
   },
   "nba": function(data, user, target) {
-    var date;
-    if (data === 'tomorrow') {
-      date = moment().tz('America/New_York').add(moment.duration(1, 'day'));
-    } else if(data.match(/^[\d]{8}$/)) {
-      date = moment(data, 'YYYYMMDD', 'America/New_York');
-    } else {
-      date = moment().tz('America/New_York');
-    }
+    var date = getDate(data);
+    var showAll = data.split(' ')[0] === '*';
     HTTP.read('http://stats.nba.com/stats/scoreboard/?LeagueID=00&DayOffset=0&gameDate=' + date.format('MM/DD/YYYY')).then(function(b) {
       var json = JSON.parse(b.toString('utf8'));
       // construct team id hash
@@ -148,17 +145,8 @@ module.exports = {
     }.bind(this));
   },
   "mlb": function(data, user, target) {
-    var date;
-    var ds = data.split(' ');
-    var showAll = ds[0] === '*';
-    var data_date = ds.pop();
-    if (data_date === 'tomorrow') {
-      date = moment().tz('America/New_York').add(moment.duration(1, 'day'));
-    } else if(data_date.match(/^[\d]{8}$/)) {
-      date = moment(data_date, 'YYYYMMDD', 'America/New_York');
-    } else {
-      date = moment().tz('America/New_York');
-    }
+    var date = getDate(data);
+    var showAll = data.split(' ')[0] === '*';
     var year = date.format('YYYY'), month = date.format('MM'), day = date.format('DD');
     var url = 'http://gd2.mlb.com/components/game/mlb/year_' + year + '/month_' + month + '/day_' + day + '/master_scoreboard.json';
     HTTP.read(url).then(function(b) {
